a392004c7a3ff80778cf24ed1f2b6698e386071f
flagrantii <tanuson679@gmail.com>
2024-03-25 18:01:12 +0700

    Edit banner image

--- src/components/TopMenuItem.tsx
+++ src/components/TopMenuItem.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function TopMenuItem({ title, pageRef }: { title: string, pageRef: string }) {
     return (
-        <Link className="w-[140px] text-center mt-auto mb-auto text-sm font-bold text-gray-500" href={pageRef}>
+        <Link className="w-[140px] text-center mt-auto mb-auto text-md font-bold text-white" href={pageRef}>
             {title}
         </Link>
     );
