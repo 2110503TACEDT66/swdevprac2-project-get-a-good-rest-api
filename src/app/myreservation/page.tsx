fc90777850d5767a5f4d6e55f76e9d1671455d3b
flagrantii <tanuson679@gmail.com>
2024-03-26 02:30:46 +0700

    combie two components in myreservation

--- src/app/myreservation/page.tsx
+++ src/app/myreservation/page.tsx
@@ -1,9 +1,11 @@
-import BookingList from "@/components/ReservationList";
+import ReservationList from "@/components/ReservationList";
+import ReservationForm from "@/components/ReservationForm";
 
 export default function MyBook() {
     return (
-        <div className="p-2">
-            <BookingList />
+        <div className="p-2 flex flex-row justify-between items-center px-24 ">
+            <ReservationList />
+            <ReservationForm isUpdate={false} id={null} />
         </div>
     )
 }
