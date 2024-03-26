78f22979552699b9611278251505a8647c6863d5
YAMAEARTH <earth0841864798@gmail.com>
2024-03-26 19:17:32 +0700

    Add Date in My Reservation

--- src/libs/getMassages.tsx
+++ src/libs/getMassages.tsx
@@ -1,18 +1,3 @@
-// export default async function getMassages() {
-    
-//     // add timeout for loading delay testing
-//     // await new Promise((resolve) => setTimeout(resolve, 1000));
-
-//     const response = await fetch(`${process.env.BACKEND_URL}/api/v1/massages`);
-//     if (!response.ok) {
-//         throw new Error("Failed to fetch massages")
-//     }
-
-
-//     return await response.json();
-// }
-
-
 export default async function getMassages() {
     // Optional loading delay simulation (uncomment if needed)
     // await new Promise((resolve) => setTimeout(resolve, 1000));
